b833044fc3107cd651f73d570f190a5605eefddd
Roger_Chen_max5211314 <rong123020990@126.com>
2019-05-20 09:48:26 +0800

    服务器部署

--- miaosha/src/main/java/com/miaoshaproject/App.java
+++ miaosha/src/main/java/com/miaoshaproject/App.java
@@ -1,8 +1,13 @@
 package com.miaoshaproject;
 
+import com.miaoshaproject.dao.UserDOMapper;
+import com.miaoshaproject.dataObject.UserDO;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Hello world!
@@ -10,13 +15,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication(scanBasePackages = {"com.miaoshaproject"})
 @MapperScan("com.miaoshaproject.dao")
+@RestController
 public class App
 {
+    @Autowired
+    UserDOMapper userDOMapper;
+
+
+    @RequestMapping("/")
+    public String home() {
+        UserDO userDO = userDOMapper.selectByPrimaryKey("1558062771294");
+        if(userDO == null) {
+            return "用户对象不存在";
+        } else {
+            return userDO.getName();
+        }
+
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println(System.currentTimeMillis());
         // 启动springboot项目
         SpringApplication.run(App.class, args);
+
     }
 }
